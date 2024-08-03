d1aebba31c941875c58967746be6490b165a7c11
vishakha <sq.vishu@gmail.com>
2024-08-03 14:59:53 +0530

    new changes

---
+++ c++/student.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int subject1=50;
+    int subject2=60;
+    int subject3=70;
+    int total=subject1+subject2+subject3;
+    cout<<"The total of three subjects is "<<total<<endl;
+    int percentage=total/3;
+    cout<<"The average of three subjects is "<<percentage<<endl;
+
+}
