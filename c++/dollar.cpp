51ebdb15c71336f75875769474769c4cd275ce06
vishakha <sq.vishu@gmail.com>
2024-08-03 14:23:25 +0530

    new changes

---
+++ c++/dollar.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int dollar=10;
+    int ruppes=82.32;
+    int dollar_in_ruppes = dollar * ruppes;
+    cout<<"$"<<dollar<<" is equal to "<<dollar_in_ruppes<<"  rupees";
+}
