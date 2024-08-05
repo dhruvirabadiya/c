7390bd6deb925cd53453c1f342994940e2cf1ce0
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2024-08-05 18:10:35 +0530

    changes

--- c++/fliaght.cpp
+++ c++/fliaght.cpp
@@ -1,54 +1,53 @@
 #include<iostream>
-
 using namespace std;
 
-class Flight{
-     private:
-     int fnumber;
-     char destination;
-     float distance;
-     float fuel;
+class Flight {
+    private:
+        int fnumber;
+        string destination;
+        float distance;
+        float fuel;
 
-     public:
-      void feedinfo();
-      void showinfo(){
-           cout<<"flight number:"<<fnumber<<endl;
-           cout<<"destination:"<<destination<<endl;
-           cout<<"distance:"<<distance<<endl;
-      }
-
-      void calfuel(){
-        if(distance<=1000){
-            fuel=500;
-        }else if(distance>=1000&&distance<=2000){
-            fuel=1100;
-        }else if(distance>=2000){
-            fuel=2200;
-        }else{
-            printf("distance not enterd correctly");
+    public:
+        void feedinfo();
+        void showinfo() {
+            cout << "Flight number: " << fnumber << endl;
+            cout << "Destination: " << destination << endl;
+            cout << "Distance: " << distance << endl;
+            cout << "Fuel: " << fuel << endl;
         }
-       
-      };
+
+        void calfuel() {
+            if (distance <= 1000) {
+                fuel = 500;
+            } else if (distance > 1000 && distance <= 2000) {
+                fuel = 1100;
+            } else if (distance > 2000) {
+                fuel = 2200;
+            } else {
+                cout << "Distance not entered correctly" << endl;
+            }
+        }
 };
 
-void Flight::feedinfo(){
-    cout<<"enter flight number:";
-    cin>>fnumber;
-    cout<<"enter destination:";
-    cin>>destination;
-    cout<<"enter distance:";
-    cin>>distance;
-};
+void Flight::feedinfo() {
+    cout << "Enter flight number: ";
+    cin >> fnumber;
+    cout << "Enter destination: ";
+    cin >> destination;
+    cout << "Enter distance: ";
+    cin >> distance;
+    calfuel();
+}
 
-
-int main(){
-    class Flight f1,f2;
+int main() {
+    Flight f1, f2;
 
     f1.feedinfo();
-    f1.calfuel();
     f1.showinfo();
 
     f2.feedinfo();
-    f2.calfuel();
     f2.showinfo();
+
+    return 0;
 }
