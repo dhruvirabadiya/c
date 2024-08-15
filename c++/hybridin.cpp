cc200dc6f2c74678ac6d7bdb6126eb18fdbe3a94
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2024-08-15 21:11:58 +0530

    hybrid inhheritance

--- c++/hybridin.cpp
+++ c++/hybridin.cpp
@@ -1,71 +1,81 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Book{
-      protected:
-      int bno;
-      char btitle[50];
-      public:
-      void input(){
-         cout<<"enter a book number:";
-         cin>>bno;
-         cout<<"enter a book title:";
-         cin>>btitle;
-        
-      }
+class Book
+{
+protected:
+  int bno;
+  char btitle[50];
 
-      void display(){
-        cout<<"book number is:"<<bno<<endl;
-        cout<<"book title is:"<<btitle<<endl;
-      }
+public:
+  void input()
+  {
+    cout << "Enter a book number: ";
+    cin >> bno;
+    cout << "Enter a book title: ";
+    cin >> btitle;
+  }
+
+  void display()
+  {
+    cout << "Book number is: " << bno << endl;
+    cout << "Book title is: " << btitle << endl;
+  }
 };
 
-class derivebook1 : public Book{
-        protected:
-         float price;
-        float total;
+class derivebook1 : public Book
+{
+protected:
+  float price;
+  float total;
 
-      float totalcst(int n){
-        return n*price;
-      }
-         public:
-        void input1(){
-            input();
-         cout<<"enter book price:";
-         cin>>price;
-      }
+  float totalcst(int n)
+  {
+    return n * price;
+  }
 
-      void display1(){
-        display();
-        cout<<"book price is:"<<price<<endl;
-      }
-        
+public:
+  void input1()
+  {
+    input();
+    cout << "Enter book price: ";
+    cin >> price;
+  }
+
+  void display1()
+  {
+    display();
+    cout << "Book price is: " << price << endl;
+  }
 };
 
-class derivebook2:public derivebook1{
-      void purchase(){
-        int n;
-        cout<<"enter  number of copies:";
-        cin>>n;
-        total=totalcst(n);
-        cout<<"total cost to be paid:"<<total<<endl;
-      }
+class derivebook2 : public derivebook1
+{
+public:
+  void purchase()
+  {
+    int n;
+    cout << "Enter number of copies: ";
+    cin >> n;
+    total = totalcst(n);
+    cout << "Total cost to be paid: " << total << endl;
+  }
 };
 
-class derivealldata:public derivebook2{
-          public:
-          void getallbookdata(){
-           input1();
-           display1();
-          }
-
+class derivealldata : public derivebook2
+{
+public:
+  void getallbookdata()
+  {
+    input1();
+    display1();
+    purchase();
+  }
 };
 
-
-      
 int main()
 {
-       derivealldata book;
-       book.getallbookdata();
-return 0;
+  derivealldata book;
+  book.getallbookdata();
+  return 0;
 }
