180faf3224fa0dbb77d432717465fb3fdf6d50d2
vishakha <sq.vishu@gmail.com>
2024-08-23 15:52:31 +0530

    dsa basic practice

---
+++ DSA/basic.c
@@ -0,0 +1,115 @@
+//AND:first digit 0 than no condition check;
+//OR:first digit 1 than no condition check
+
+#include<stdio.h>
+
+int main(){
+    // int a=0,b=1,c=1,d;
+
+    // d=++a&&++b||c;
+    // printf("a: %d,b: %d,c: %d,d: %d",a,b,c,d);
+
+    // d=a++&&++b&&c;
+    // printf("a: %d,b: %d,c: %d,d: %d",a,b,c,d);
+
+    // int a=1,b=2,c;
+
+    // c=++a||++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a++||++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //c=a++||b++;
+    //printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a||++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=++a||b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=a++||b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a||b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=a||b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=--a||++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=++a||--b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a--||b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a++||b--;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=--a||b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a||b--;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a++||b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a||b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    int a=1,b=2,c;
+
+    // c=++a&&++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=++a&&b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a++&&b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a&&++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c = ++a && b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=a++&&b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a&&b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=a&&b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=--a&&++b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    //  c=++a&&--b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a--&&b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a++&&b--;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=--a&&b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a&&b--;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a++&&b;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+    // c=a&&b++;
+    // printf("a: %d,b: %d,c: %d",a,b,c);
+
+}
---
+++ DSA/basic1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int a=15;
+
+    //  printf("a: %d,a: %d,a: %d,a: %d",a++,++a,a++,++a);
+    //  printf("a: %d,a: %d,a: %d,a: %d",++a,a++,++a,a++);
+    //  printf("a: %d,a: %d,a: %d,a: %d",a,a++,a++,++a);
+    // printf("a: %d,a: %d,a: %d,a: %d",a++,a,++a,a);
+    //  printf("a: %d,a: %d,a: %d",++a,++a,++a);
+    //  printf("a: %d,a: %d,a: %d",a++,a++,a++);
+    //  printf("a: %d,a: %d,a: %d",a++,++a,a++);
+    // printf("a: %d,a: %d,a: %d",++a,a++,++a);
+    //  printf("a: %d,a: %d,a: %d",a,a++,a++);
+    //  printf("a: %d,a: %d,a: %d",a,++a,++a);
+    //  printf("a: %d,a: %d",++a,++a);
+    //  printf("a: %d,a: %d",a++,a++);
+    //  printf("a: %d,a: %d",++a,a++);
+    //  printf("a: %d,a: %d",a++,a++);
+    //  printf("a: %d,a: %d",a,a++);
+    //  printf("a: %d",++a);
+    //  printf("a: %d",a++);
+
+
+
+
+
+
+}
