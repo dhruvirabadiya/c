5c0af6c886e396958e4662d6ce722c65e8a1a042
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2024-08-27 22:30:49 +0530

    dsa

--- DSA/mergetwoarry.c
+++ DSA/mergetwoarry.c
@@ -4,35 +4,29 @@ int main()
 {
    int n, n1, n2;
 
-   // Input size of first array
    printf("Enter the value of n (size of first array): ");
    scanf("%d", &n);
-   int a[n]; // Declaring array 'a' with size 'n'
+   int a[n];
 
-   // Input elements of first array
    for (int i = 0; i < n; i++)
    {
       printf("Enter the value of array1 at position %d: ", i);
       scanf("%d", &a[i]);
    }
 
-   // Input size of second array
    printf("Enter the value of n1 (size of second array): ");
    scanf("%d", &n1);
-   int a1[n1]; // Declaring array 'a1' with size 'n1'
+   int a1[n1];
 
-   // Input elements of second array
    for (int j = 0; j < n1; j++)
    {
       printf("Enter the value of array2 at position %d: ", j);
       scanf("%d", &a1[j]);
    }
 
-   // Size of the merged array
    n2 = n + n1;
-   int a2[n2]; // Declaring merged array 'a2'
+   int a2[n2];
 
-   // Merging array 'a' and 'a1' into 'a2'
    for (int i = 0; i < n; i++)
    {
       a2[i] = a[i];
@@ -43,7 +37,6 @@ int main()
       a2[n + j] = a1[j];
    }
 
-   // Printing the merged array
    printf("Merged array is: ");
    for (int i = 0; i < n2; i++)
    {
--- DSA/reversepos.c
+++ DSA/reversepos.c
@@ -7,21 +7,18 @@ int main()
      printf("Enter the value of n: ");
      scanf("%d", &n);
 
-     int arr[n]; // Declaring the array after reading n
+     int arr[n];
 
-     // Input array elements
      for (int i = 0; i < n; i++)
      {
           printf("Enter the array element at index %d: ", i);
           scanf("%d", &arr[i]);
      }
 
-     // Swapping the first and last elements
      int temp = arr[0];
      arr[0] = arr[n - 1];
      arr[n - 1] = temp;
 
-     // Printing the array after swapping
      printf("Array after swapping: ");
      for (int i = 0; i < n; i++)
      {
