5136003deadbe5b36f64f54e2296477c8d254ced
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2024-08-27 22:21:10 +0530

    dsa

--- c++/constructor1.cpp
+++ c++/constructor1.cpp
@@ -1,55 +1,59 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class Student{
+class Student
+{
 
-    private:
+private:
     int admno;
     char sname[20];
-    float eng,maths,science;
+    float eng, maths, science;
     float total;
 
-    public:
-    Student(){
-    cout<<"enter admno:";
-     cin>>admno;
-     cout<<"enter student name:";
-     cin>>sname;
-     cout<<"enter marks of english:";
-     cin>>eng;
-     cout<<"enter marks of maths:";
-     cin>>maths;
-     cout<<"enter marks of science:";
-     cin>>science;
+public:
+    Student()
+    {
+        cout << "enter admno:";
+        cin >> admno;
+        cout << "enter student name:";
+        cin >> sname;
+        cout << "enter marks of english:";
+        cin >> eng;
+        cout << "enter marks of maths:";
+        cin >> maths;
+        cout << "enter marks of science:";
+        cin >> science;
     }
 
-    void showdata(){
-        cout<<"admno:"<<admno<<endl;
-        cout<<"name:"<<sname<<endl;
-        cout<<"english:"<<eng<<endl;
-        cout<<"maths:"<<maths<<endl;
-        cout<<"science:"<<science<<endl;
+    void showdata()
+    {
+        cout << "admno:" << admno << endl;
+        cout << "name:" << sname << endl;
+        cout << "english:" << eng << endl;
+        cout << "maths:" << maths << endl;
+        cout << "science:" << science << endl;
     }
 
-    void ctotal(){
+    void ctotal()
+    {
         total = eng + maths + science;
-        cout<<"total:"<<total<<endl;
-        
+        cout << "total:" << total << endl;
     }
 
-    ~Student(){
-        cout<<"destructor is destroy:";
+    ~Student()
+    {
+        cout << "destructor is destroy:";
     }
 };
 
-int main(){
-     Student s1,s2;
-   
+int main()
+{
+    Student s1, s2;
+
     s1.showdata();
     s1.ctotal();
- 
+
     s2.showdata();
     s2.ctotal();
-    
 }
