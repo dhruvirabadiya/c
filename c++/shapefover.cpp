c7897e1736cc71249691c4a36a89ad690c074c89
vishakha <sq.vishu@gmail.com>
2024-08-29 01:17:50 +0530

    changes

--- c++/shapefover.cpp
+++ c++/shapefover.cpp
@@ -7,22 +7,22 @@ class Shape{
     float pie=3.14;
 
     public:
-    void circleshape(double r){
+    void shape(double r){
            circle=pie*r*r;
            cout<<"volume of circle are:"<<circle<<endl;
     }
 
-    void squareshape(int side){
+    void shape(int side){
        square=side*side;
        cout<<"volume of square is:"<<square<<endl;
     }
 
-   void rectangleshape(int length,int width){
+   void shape(int length,int width){
         rectangle=length*width;
         cout<<"volume of rectangle is:"<<rectangle<<endl;
    }
 
-   void triangleshape(double base,double height){
+   void shape(double base,double height){
      triangle=base*height/2;
      cout<<"volume of triangle is:"<<triangle<<endl;
     }
@@ -34,10 +34,10 @@ int main()
 {
 
      Shape s1;
-     s1.circleshape(20);
-     s1.squareshape(20);
-     s1.rectangleshape(30,20);
-     s1.triangleshape(40.0,50.0);
+     s1.shape(20);
+     s1.shape(20);
+     s1.shape(30,20);
+     s1.shape(40.0,50.0);
        
 return 0;
 }
