81a2febc48c1a8d780e24b8999cf32c42fb79d69
vishakha <sq.vishu@gmail.com>
2024-08-28 14:07:31 +0530

    dsa

--- DSA/reversepos.c
+++ DSA/reversepos.c
@@ -12,11 +12,10 @@ int main(){
          scanf("%d",&arr[n]);
     }
 
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n/2;i++){
          temp=arr[i];
          arr[i]=arr[n-i-1];
          arr[n-i-1]=temp;
          printf("after swaping is: %d",arr[i]);
-
     }
 }
