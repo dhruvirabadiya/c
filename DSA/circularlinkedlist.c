3b09ef5bce7b9d7ff1cc90261f1092d466cba2ea
dhruvirabadiya <140322854+dhruvirabadiya@users.noreply.github.com>
2024-09-13 16:24:06 +0530

    linkedlist

--- DSA/circularlinkedlist.c
+++ DSA/circularlinkedlist.c
@@ -8,65 +8,77 @@ struct node
 };
 
 struct node *head = NULL;
-
-void insert_end(int val)
+struct node *tail = NULL;
+void Insert_End(int val)
 {
-    struct node *ptr = head;
     struct node *temp = malloc(sizeof(struct node));
     temp->data = val;
-    temp->next = NULL;
-
-    if (head == NULL)
+    temp->next = head;
+    if (head == NULL && tail == NULL) 
     {
         head = temp;
+        tail = temp;
         return;
     }
-    while (ptr->next != NULL)
-    {
-        ptr = ptr->next;
-    }
-
-    ptr->next = temp;
-    return;
+    tail->next = temp;
+    tail = temp;
 }
 
-void delete_end()
+void Delete_end()
 {
-    struct node *ptr = head;
-    struct node *p;
-
-    if (head->next == NULL)
+    struct node *ptr = tail;
+    struct node *p = head;
+    if (head == NULL)
     {
-        head = NULL;
+        printf("\nList is Empty......\n");
+        return;
+    }
+    else if (head == tail)
+    {
+        head == NULL;
+        tail == NULL;
         free(ptr);
         return;
     }
-    while (ptr->next != NULL)
+    while (p->next != ptr)
     {
-        p = ptr;
-        ptr = ptr->next;
+        p = p->next;
     }
-    p->next = NULL;
+    p->next = head;
+    tail = p;
     free(ptr);
-    return;
 }
 
-void insert_first(int val)
+void Insert_first(int val)
 {
-    struct node *ptr = head;
+
     struct node *temp = malloc(sizeof(struct node));
     temp->data = val;
     temp->next = head;
     head = temp;
+    tail->next = head;
 }
-void delete_first()
+
+void Delete_first()
 {
+    if (head == NULL)
+    {
+        printf("\nList is Empty......\n");
+    }
+
+    if (head == tail)
+    {
+        head = NULL;
+        tail = NULL;
+        return;
+    }
     struct node *ptr = head;
     head = ptr->next;
+    tail->next = head;
     free(ptr);
 }
 
-void insert_mid(int num, int pos)
+void Insert_mid(int num, int pos)
 {
     struct node *ptr = head;
     struct node *p;
@@ -85,7 +97,7 @@ void insert_mid(int num, int pos)
     temp->next = ptr;
 }
 
-void delete_mid(int pos)
+void Delete_mid(int pos)
 {
     struct node *ptr = head;
     struct node *p;
@@ -98,22 +110,23 @@ void delete_mid(int pos)
     free(ptr);
 }
 
-void display()
+void Display()
 {
     struct node *ptr = head;
     if (head == NULL)
     {
-        printf("The List Is Already Empty...");
+        printf("List Is Already Empty....!");
     }
     else
     {
-        while (ptr != NULL)
+        while (ptr->next != head)
         {
             printf("%d \t", ptr->data);
             ptr = ptr->next;
         }
+        printf("%d \n", ptr->data);
     }
-    printf("\n \n");
+    printf("\n");
 }
 
 int main()
@@ -123,78 +136,74 @@ int main()
     {
         printf("\n Press 1 For Insert End");
         printf("\n Press 2 For Delete End");
-        printf("\n Press 3 for Insert First");
+        printf("\n Press 3 For Insert First");
         printf("\n Press 4 for Delete First");
-        printf("\n Press 5 for Insert Mid");
-        printf("\n Press 6 for Delete Mid");
-        printf("\n Press 7 for Display");
+        printf("\n Press 5 For Insert Mid");
+        printf("\n Press 6 For Delete Mid");
+        printf("\n Press 7 For Display");
         printf("\n");
-        printf("Enter Your choice:");
+
+        printf("Enter Your Choice:");
         scanf("%d", &n);
 
-        if (n <= 0 || n > 7)
+        if (n <= 0 && n >= 7)
         {
-            printf("\n Invalid Input!!!! \n");
+            printf("Invalid Input.... \n");
         }
 
         if (n == 1)
         {
             int num;
-            printf("Enter The Number You Want To Add In The List:");
+            printf("Enter a Number You Want To Add In List:");
             scanf("%d", &num);
-            insert_end(num);
+            Insert_End(num);
             printf("Node inserted \n");
         }
-
         else if (n == 2)
         {
-            delete_end();
-            printf("Node deleted \n");
+            Delete_end();
+             printf("Node deleted \n");
         }
-
         else if (n == 3)
         {
             int num;
-            printf("Enter The Number You Want To Add In The List:");
+            printf("Enter The Number You Want To Add In List:");
             scanf("%d", &num);
-            insert_first(num);
-            printf("Node inserted at begining \n");
+            Insert_first(num);
+             printf("Node inserted at begining \n");
         }
-
         else if (n == 4)
         {
-            delete_first();
-            printf("Node deleted begining\n");
+            Delete_first();
+              printf("Node deleted begining\n");
         }
-
         else if (n == 5)
         {
             int num;
-            printf("Enter Position Where You Can Add Your New Number:");
+            printf("Enter A Position Where You Can Add Your New Number:");
             scanf("%d", &pos);
+            Display();
 
-            display();
-            printf("Enter Number You Want To Add In The List:");
+            printf("Enter A Number You Want To Add In List:");
             scanf("%d", &num);
 
-            insert_mid(num, pos);
-            display();
+            Insert_mid(pos, num);
+            Display();
             printf("Node inserted at middle \n");
         }
-
         else if (n == 6)
         {
-            printf("Enter The Position Where You Can Delete Your Data:");
+            printf("Enter A Position Where You Can Add Data:");
             scanf("%d", &pos);
-            delete_mid(pos);
-            display();
+            Delete_mid(pos);
+            Display();
             printf("Node deleted at middle\n");
         }
-
         else if (n == 7)
         {
             printf("\nprinting values . . . . .\n");
-            display();
+            Display();
         }
+
     } while (n > 0 || n <= 7);
 }
